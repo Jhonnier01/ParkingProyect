132c11ab7207073fc55f81a378f3db12acb89fbc
Jhonnier01 <joniergarcia2@gmail.com>
2022-11-23 16:01:48 -0500

    Final
    Final commit

--- ParkingProyect/src/module-info.java
+++ ParkingProyect/src/module-info.java
@@ -4,6 +4,8 @@ module ParkingProyect {
 	requires javafx.graphics;
 	requires java.sql;
 	requires javafx.base;
+	requires java.desktop;
+	requires junit;
 	
 	opens co.edu.poli.parkingproyect.controller to javafx.graphics, javafx.fxml;
 	opens co.edu.poli.parkingproyect.model to javafx.graphics, javafx.fxml;
